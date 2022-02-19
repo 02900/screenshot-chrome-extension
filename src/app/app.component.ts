ad65d3ef34b171219974c4e7b5a55b1b8e192eef
Juan Ortiz <juan.ortiz@kavak.com>
2022-02-18 23:25:11 -0600

    feat: iterate over devices taking screenshots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { resolutions } from './resolutions';
 
 const DEBUGGING_PROTOCOL_VERSION = '1.0';
 const methodEmulateDevice = 'Emulation.setDeviceMetricsOverride';
 const methodScreenshot = 'Page.captureScreenshot';
 const urlPrefix = 'data:application/octet-stream;base64,';
+const delay = 800;
 
 @Component({
   selector: 'app-root',
@@ -11,39 +13,37 @@ const urlPrefix = 'data:application/octet-stream;base64,';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  readonly iphone5 = {
-    id: 'iPhone5',
-    mobile: true,
-    width: 320,
-    height: 568,
-    deviceScaleFactor: 1,
-  };
+  private readonly ext = 'png';
+  private readonly timer = (ms: number) => new Promise(res => setTimeout(res, ms))
 
-  readonly ext = 'png';
+  async takeScreenshots() {
+    for (let i = 0; i < resolutions.length; i++) {
+      const resolution = resolutions[i];
 
-  resize() {
-    chrome.tabs.query({ active: true }, (tabs) => {
-      const tabId = { tabId: tabs[0].id };
-      chrome.debugger.attach(tabId, DEBUGGING_PROTOCOL_VERSION);
-      chrome.debugger.sendCommand(tabId, methodEmulateDevice, this.iphone5, () => {
-        chrome.debugger.sendCommand(
-          tabId,
-          methodScreenshot,
-          { format: this.ext, fromSurface: true },
-          (response: any) => {
-            if (chrome.runtime.lastError) {
+      chrome.tabs.query({ active: true }, (tabs) => {
+        const tabId = { tabId: tabs[0].id };
+        chrome.debugger.attach(tabId, DEBUGGING_PROTOCOL_VERSION);
+        chrome.debugger.sendCommand(tabId, methodEmulateDevice, resolution, () => {
+          chrome.debugger.sendCommand(
+            tabId,
+            methodScreenshot,
+            { format: this.ext, fromSurface: true },
+            (response: any) => {
+              if (chrome.runtime.lastError) {
+                chrome.debugger.detach(tabId);
+                return;
+              }
+
+              chrome.downloads.download({
+                filename: `${resolution.id}.${this.ext}`,
+                url: `${urlPrefix}${response.data}`,
+              });
               chrome.debugger.detach(tabId);
-              return;
             }
-
-            chrome.downloads.download({
-              filename: `${this.iphone5.id}.${this.ext}`,
-              url: `${urlPrefix}${response.data}`,
-            });
-            chrome.debugger.detach(tabId);
-          }
-        );
+          );
+        });
       });
-    });
+      await this.timer(delay);
+    }
   }
 }
---
+++ src/app/resolutions.ts
@@ -0,0 +1,65 @@
+const iphone5 = {
+  id: 'iPhone5',
+  mobile: true,
+  width: 320,
+  height: 568,
+  deviceScaleFactor: 1,
+};
+
+const iphoneXR = {
+  id: 'iPhoneXR',
+  mobile: true,
+  width: 414,
+  height: 896,
+  deviceScaleFactor: 1,
+};
+
+const iPadMini = {
+  id: 'iPadMini',
+  mobile: true,
+  width: 768,
+  height: 1024,
+  deviceScaleFactor: 1,
+};
+
+const desktopLG = {
+  id: 'desktopLG',
+  mobile: false,
+  width: 1024,
+  height: 768,
+  deviceScaleFactor: 1,
+};
+
+const desktopXL = {
+  id: 'desktopXL',
+  mobile: false,
+  width: 1366,
+  height: 968,
+  deviceScaleFactor: 1,
+};
+
+const desktopXXL = {
+  id: 'desktopXXL',
+  mobile: false,
+  width: 1440,
+  height: 1024,
+  deviceScaleFactor: 1,
+};
+
+const desktopXXXL = {
+  id: 'desktopXXXL',
+  mobile: false,
+  width: 1920,
+  height: 1080,
+  deviceScaleFactor: 1,
+};
+
+export const resolutions = [
+  iphone5,
+  iphoneXR,
+  iPadMini,
+  desktopLG,
+  desktopXL,
+  desktopXXL,
+  desktopXXXL,
+];
